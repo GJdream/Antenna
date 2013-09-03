951e739b028c213da963839236589fc8b8367c7b
Cory D. Wiles <kwylez@gmail.com>
2013-09-03 16:53:10 -0400

    initial commit

---
+++ Example/Antenna Tests/Antenna_Tests.m
@@ -0,0 +1,78 @@
+//
+//  Antenna_Tests.m
+//  Antenna Tests
+//
+//  Created by Cory D. Wiles on 9/3/13.
+//  Copyright (c) 2013 Mattt Thompson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "Antenna.h"
+
+static NSString * const AntennaTestsExampleNotification = @"AntennaTestsExampleNotification";
+
+@interface Antenna_Tests : SenTestCase
+
+- (void)channelWasAddedNotification:(NSNotification *)aNotif;
+- (void)channelWasRemovedNotification:(NSNotification *)aNotif;
+
+@end
+
+@implementation Antenna_Tests
+
+- (void)setUp {
+  
+    [super setUp];
+  
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(channelWasAddedNotification:)
+                                                 name:AntennaChannelAddedNotification
+                                               object:nil];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(channelWasRemovedNotification:)
+                                                 name:AntennaChannelRemovedNotification
+                                               object:nil];
+
+    [[Antenna sharedLogger] startLoggingApplicationLifecycleNotifications];
+    [[Antenna sharedLogger] startLoggingNotificationName:AntennaTestsExampleNotification];
+}
+
+- (void)tearDown {
+
+    [super tearDown];
+  
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)test_000_Successfully_Add_Channel {
+  
+    [[Antenna sharedLogger] addChannelWithURL:[NSURL URLWithString:@"http://localhost:5000"]
+                                       method:@"LOG"
+                                      forName:@"defaultLog"];
+}
+
+- (void)test_001_Successfully_Remove_Channel {
+  [[Antenna sharedLogger] removeChannelForName:@"defaultLog"];
+}
+
+#pragma mark - Notification Handlers
+
+- (void)channelWasAddedNotification:(NSNotification *)aNotif {
+
+  NSString *channelName = [aNotif userInfo][AntennaChannelNotificationDictKey];
+  
+  STAssertNotNil(channelName, @"Notification name can't be nil");
+}
+
+- (void)channelWasRemovedNotification:(NSNotification *)aNotif {
+  
+  NSString *deletedChannelName = [aNotif userInfo][AntennaChannelNotificationDictKey];
+  NSString *errorMsg           = [NSString stringWithFormat:@"The channel (%@) should have been deleted", deletedChannelName];
+
+  id<AntennaChannel> channelObj = [[Antenna sharedLogger] channelForName:deletedChannelName];
+
+  STAssertNil(channelObj, errorMsg);
+}
+
+@end
