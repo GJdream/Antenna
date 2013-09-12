51a70940b190ae8e682e070535a6a2600d7018e2
Cory D. Wiles <kwylez@gmail.com>
2013-09-11 20:43:23 -0400

    code doc

--- Tesla/Tesla.h
+++ Tesla/Tesla.h
@@ -22,6 +22,8 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NSDictionary *(^TeslaPayloadConstructionBlock)(NSNotification *notification);
+
 extern NSString * const TeslaChannelAddedNotification;
 extern NSString * const TeslaChannelRemovedNotification;
 extern NSString * const TeslaChannelNotificationDictKey;
@@ -32,139 +34,216 @@ extern NSString * const TeslaFilesSubDirectoryName;
 @protocol TeslaChannel <NSObject>
 
 /**
- 
+ * Should perform transformation of dictionary data into a json string and passes it to
+ * logEvent:
+ *
+ * @see logEvent:
+ * @todo
+ * Feels redudant to have both of these messages. Should only be 1?
  */
+
 - (void)log:(NSDictionary *)payload;
 
 @optional
+
+/**
+ * Should log the string to whatever medium that the class wants
+ */
 
 - (void)logEvent:(NSString *)eventMessage;
 
 @end
 
 /**
- 
+ * An instance of Tesla provides the gateway to centralized event logging.
+ * After registering an adapter (any class that conforms to TeslaChannel) 
+ * all the user has to do is then call any of the logEventMessage* methods.
+ *
+ * There is built in support for application lifecycle notifications.
  */
+
 @interface Tesla : NSObject
 
 /**
- 
+ * Standard key/value pairs that are added to the log. These include:
+ * - uuid
+ * - locale
+ * - currentTimestamp
+ * - channelName
  */
+
 @property (readonly, nonatomic, strong) NSMutableDictionary *defaultPayload;
 
 /**
- 
+ * System notification center
  */
+
 @property (nonatomic, strong) NSNotificationCenter *notificationCenter;
 
 /**
- 
+ * Operation queue that notifications are returned on
  */
+
 @property (readonly, nonatomic, strong) NSOperationQueue *operationQueue;
 
 /**
- 
+ * Singleton instance of Tesla class
+ * @return Tesla
  */
+
 + (instancetype)sharedLogger;
+
+/**
+ * Class method that returns the path to the log directory for Tesla.
+ * This is subdirectory inside of /tmp so it is never backed up to iTunes or 
+ * iCloud.
+ *
+ * @return NSString
+ */
 
 + (NSString *)logTempDirectory;
 
-///======================
-/// @name Adding Channels
-///======================
 
 /**
- 
+ * Adds channel to list of avaiable channels that can be logged to. The name of the 
+ * channel is the key in the channels dictionary.
+ *
+ * @param id object that conforms to the TeslaChannel protocol
+ * @param string name of the channel
  */
+
 - (void)addChannel:(id <TeslaChannel>)channel forName:(NSString *)name;
 
 /**
- 
+ * Removes channel from list of avaiable channels that can be logged to. The name of the
+ * channel is the key in the channels dictionary.
+ *
+ * @param string name of the channel to add
  */
+
 - (void)removeChannelForName:(NSString *)name;
 
 /**
- 
+ * Checks for channel existance
+ *
+ * @return BOOL
+ * @param string name of the channel to remove
  */
 - (BOOL)channelExists:(NSString *)name;
 
 /**
- 
+ * Channel object for a given name
+ *
+ * @return id channel that conforms to TeslaChannel protocol
+ * @param string name of channel
  */
+
 - (id <TeslaChannel>)channelForName:(NSString *)name;
 
 /**
- 
+ * Iterates over the Tesla temp directory and returns list of all the files.
+ *
+ * @return NSArray
  */
+
 + (NSArray *)pendingFiles;
 
 /**
- 
+ * Adds a channel that will connect with url. This is usually used inconjunction 
+ * with a webservices, but could be file url.
+ *
+ * @param nsurl url to resource
+ * @param string HTTP method to use. For example PUT or POST
+ * @param string name of channel to "write" to
  */
+
 - (void)addChannelWithURL:(NSURL *)URL method:(NSString *)method forName:(NSString *)name;
 
 /**
- * @name logEventMessage
- * @param id messageOrPayload
- *
  * This is the _main_ log method for Tesla. It check and see
  * `[obj respondsToSelector:@selector(log:)]` to make sure that the channel obj
  * will call the delegate method `log`. Due to the confusing nature of method
  * names this should be refactored.
  *
- * @todo
- * We give a channel by name, but yet we log to all channels. This method should
- * ideally accept a payload and array of channels names. If not then what's the
- * point of having names or this _could_ be used as general message to just log
- * all channels
+ * @param id object or NSDictionary that should be logged
  */
+
 - (void)logEventMessage:(id)messageOrPayload;
 
 /**
- 
+ * This is the _main_ log method for Tesla. It check and see
+ * `[obj respondsToSelector:@selector(log:)]` to make sure that the channel obj
+ * will call the delegate method `log`. Due to the confusing nature of method
+ * names this should be refactored.
+ *
+ * @param id messageOrPayload
+ * @param nsarray list of channel names
  */
+
 - (void)logEventMessage:(id)messageOrPayload forChannelNames:(NSArray *)names;
 
 /**
- 
+ * This is the _main_ log method for Tesla. It check and see
+ * `[obj respondsToSelector:@selector(log:)]` to make sure that the channel obj
+ * will call the delegate method `log`. Due to the confusing nature of method
+ * names this should be refactored.
+ *
+ * @param id messageOrPayload
+ * @param string channel name
  */
+
 - (void)logEventMessage:(id)messageOrPayload forChannelName:(NSString *)name;
 
-///===========================
-/// @name Notification Logging
-///===========================
-
 /**
- 
+ * Method that kicks off logging for app delegate lifecycle notifications
  */
+
 - (void)startLoggingApplicationLifecycleNotifications;
 
 /**
- 
+ * Method that kicks off logging for app delegate lifecycle notifications for a 
+ * given name
+ *
+ * @param string Notification name
  */
+
 - (void)startLoggingNotificationName:(NSString *)name;
 
 /**
-
+ * Method that kicks off logging for app delegate lifecycle notifications for a
+ * given name and object
+ *
+ * @param string Notification name
+ * @param object
  */
-- (void)startLoggingNotificationName:(NSString *)name
-                              object:(id)object;
+
+- (void)startLoggingNotificationName:(NSString *)name object:(id)object;
 
 /**
- 
+ * Method that kicks off logging for app delegate lifecycle notifications for a
+ * given name, object and notification payload
+ *
+ * @param string Notification name
+ * @param object
+ * @param TeslaPayloadConstructionBlock returns an NSDictionary
  */
+
 - (void)startLoggingNotificationName:(NSString *)name
                               object:(id)object
-        constructingPayLoadFromBlock:(NSDictionary * (^)(NSNotification *notification))block;
+        constructingPayLoadFromBlock:(TeslaPayloadConstructionBlock)block;
 
 /**
- 
+ * Removes notification for a given name
+ *
+ * @param string name of notification
  */
+
 - (void)stopLoggingNotificationName:(NSString *)name;
 
 /**
- 
+ * Removes all notifications
  */
+
 - (void)stopLoggingAllNotifications;
 
 
